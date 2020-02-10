e66eb6db55bc37dda70104e7eef7eb5d59262234
kyleclo <kyleclo@uw.edu>
2020-02-10 00:23:07 -0800

    evaluate lm

---
+++ examples/eval_language_modeling.py
@@ -0,0 +1,140 @@
+"""
+
+Just load and evaluate a language model
+
+"""
+
+
+
+import argparse
+import glob
+import logging
+import os
+import pickle
+import random
+import re
+import shutil
+from typing import Dict, List, Tuple
+
+import numpy as np
+import torch
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+from tqdm import tqdm, trange
+
+from transformers import (
+    WEIGHTS_NAME,
+    AdamW,
+    BertConfig,
+    BertForMaskedLM,
+    BertTokenizer,
+    CamembertConfig,
+    CamembertForMaskedLM,
+    CamembertTokenizer,
+    DistilBertConfig,
+    DistilBertForMaskedLM,
+    DistilBertTokenizer,
+    GPT2Config,
+    GPT2LMHeadModel,
+    GPT2Tokenizer,
+    OpenAIGPTConfig,
+    OpenAIGPTLMHeadModel,
+    OpenAIGPTTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    RobertaConfig,
+    RobertaForMaskedLM,
+    RobertaTokenizer,
+    get_linear_schedule_with_warmup,
+)
+
+
+logger = logging.getLogger(__name__)
+
+
+from examples.run_language_modeling import MODEL_CLASSES, TextDataset, LineByLineTextDataset, load_and_cache_examples, \
+    set_seed, evaluate
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="The output directory where the model predictions and checkpoints will be written.",
+    )
+    parser.add_argument(
+        "--model_type", type=str, required=True, help="The model architecture to be trained or fine-tuned.",
+    )
+
+    # Other parameters
+    parser.add_argument(
+        "--eval_data_file",
+        default=None,
+        type=str,
+        help="An optional input evaluation data file to evaluate the perplexity on (a text file).",
+    )
+    parser.add_argument(
+        "--line_by_line",
+        action="store_true",
+        help="Whether distinct lines of text in the dataset are to be handled as distinct sequences.",
+    )
+    parser.add_argument(
+        "--model_name_or_path",
+        default=None,
+        type=str,
+        help="The model checkpoint for weights initialization. Leave None if you want to train a model from scratch.",
+    )
+
+    parser.add_argument("--per_gpu_eval_batch_size", default=4, type=int, help="Batch size per GPU/CPU for evaluation.")
+
+    parser.add_argument(
+        "--eval_all_checkpoints",
+        action="store_true",
+        help="Evaluate all checkpoints starting with the same prefix as model_name_or_path ending and ending with step number",
+    )
+    parser.add_argument("--no_cuda", action="store_true", help="Avoid using CUDA when available")
+    parser.add_argument("--seed", type=int, default=42, help="random seed for initialization")
+
+    parser.add_argument("--local_rank", type=int, default=-1, help="For distributed training: local_rank")
+    args = parser.parse_args()
+
+    # Setup CUDA, GPU.   No distributed evaluation.
+    assert args.local_rank == -1 or args.no_cuda
+    device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    args.n_gpu = torch.cuda.device_count()
+    args.device = device
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO if args.local_rank in [-1, 0] else logging.WARN,
+    )
+
+    # Set seed
+    set_seed(args)
+
+    config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
+
+    # evaluate
+    results = {}
+    checkpoints = [args.output_dir]
+    if args.eval_all_checkpoints:
+        checkpoints = list(os.path.dirname(c) for c in sorted(glob.glob(args.output_dir + "/**/" + WEIGHTS_NAME, recursive=True)))
+        logging.getLogger("transformers.modeling_utils").setLevel(logging.WARN)  # Reduce logging
+    logger.info("Evaluate the following checkpoints: %s", checkpoints)
+    for checkpoint in checkpoints:
+        global_step = checkpoint.split("-")[-1] if len(checkpoints) > 1 else ""
+        prefix = checkpoint.split("/")[-1] if checkpoint.find("checkpoint") != -1 else ""
+
+        model = model_class.from_pretrained(checkpoint)
+        tokenizer = tokenizer_class.from_pretrained(checkpoint)
+        args.block_size = tokenizer.max_len_single_sentence
+        model.to(args.device)
+        result = evaluate(args, model, tokenizer, prefix=prefix)
+        result = dict((k + "_{}".format(global_step), v) for k, v in result.items())
+        results.update(result)
+
