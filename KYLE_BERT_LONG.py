178ab46bdb806dc3168db64c6f5de9099f199288
kyleclo <kyleclo@uw.edu>
2020-02-25 16:09:28 -0800

    notes

--- KYLE_BERT_LONG.py
+++ KYLE_BERT_LONG.py
@@ -10,6 +10,15 @@ from src.transformers import RobertaModel, RobertaTokenizer
 
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 model = RobertaModel.from_pretrained('roberta-base')
-input_ids = torch.tensor(tokenizer.encode("Hello, my dog is cute")).unsqueeze(0)  # Batch size 1
-outputs = model(input_ids)
+encoding = tokenizer.encode("Hello, my dog is cute")
+decoding = tokenizer.decode(encoding)
+assert decoding == '<s>Hello, my dog is cute</s>'
+T1 = torch.tensor(encoding)  # dimension 8
+T2 = T1.unsqueeze(0)  # dimensions (1 x 8)
+outputs = model(T2)  # tuple of 2 elements
+outputs[0].shape    # dimensions (1 x 8 x 768)
+outputs[1].shape    # dimensions (1 x 768)
 last_hidden_states = outputs[0]  # The last hidden-state is the first element of the output tuple
+
+
+
